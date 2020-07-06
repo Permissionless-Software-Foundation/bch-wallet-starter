38bbd24c87ac840b7565dd76db944763bfcb5e56
Chris Troutner <chris.troutner@gmail.com>
2020-07-06 15:09:28 -0700

    Adding more comments to the menu-components.js

--- src/gatsby-ipfs-web-wallet/components/menu-components.js
+++ src/gatsby-ipfs-web-wallet/components/menu-components.js
@@ -1,3 +1,11 @@
+/*
+  This file is how you add new menu items to your site. It uses the Gatsby
+  concept of Component Shadowing:
+  https://www.gatsbyjs.org/blog/2019-04-29-component-shadowing/
+
+  It over-rides he default file in the gatsby-ipfs-web-wallet Theme.
+*/
+
 import React from "react"
 import { Sidebar } from "adminlte-2-react"
 
