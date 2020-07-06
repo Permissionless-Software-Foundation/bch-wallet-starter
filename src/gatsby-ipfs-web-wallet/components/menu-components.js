22a9f29274b32429cc395995f94b7242a023d129
Chris Troutner <chris.troutner@gmail.com>
2020-07-06 15:13:35 -0700

    fix(linting): Adding standard JS linting rules

--- src/gatsby-ipfs-web-wallet/components/menu-components.js
+++ src/gatsby-ipfs-web-wallet/components/menu-components.js
@@ -6,17 +6,17 @@
   It over-rides he default file in the gatsby-ipfs-web-wallet Theme.
 */
 
-import React from "react"
-import { Sidebar } from "adminlte-2-react"
+import React from 'react'
+import { Sidebar } from 'adminlte-2-react'
 
 const { Item } = Sidebar
 
 const MenuComponents = [
   {
-    key: "New Component",
+    key: 'New Component',
     component: <div>Test!</div>,
-    menuItem: <Item icon="fas-cog" key="New Component" text="New Component" />,
-  },
+    menuItem: <Item icon='fas-cog' key='New Component' text='New Component' />
+  }
 ]
 
 export default MenuComponents
