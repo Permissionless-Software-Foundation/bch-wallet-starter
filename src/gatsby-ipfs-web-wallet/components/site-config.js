230133ec79b9ecc926f6e7c22f4185d75ab617c1
Chris Troutner <chris.troutner@gmail.com>
2020-07-07 10:38:19 -0700

    Adding site-config file

---
+++ src/gatsby-ipfs-web-wallet/components/site-config.js
@@ -0,0 +1,13 @@
+/*
+  This file is intended to be overwritten. It provides a common place to store
+  site configuration data.
+*/
+
+const config = {
+  title: 'Wallet Demo',
+  titleShort: 'Demo',
+  balanceText: 'BCH Balance',
+  balanceIcon: 'fab-bitcoin'
+}
+
+module.exports = config
