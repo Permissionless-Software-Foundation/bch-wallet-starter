e7b29fb8fcd6efcd6a3b0d213cdf6240aaa501ba
Chris Troutner <chris.troutner@gmail.com>
2020-10-19 14:54:05 -0700

    linting

--- src/gatsby-ipfs-web-wallet/components/menu-components.js
+++ src/gatsby-ipfs-web-wallet/components/menu-components.js
@@ -31,42 +31,42 @@ const MenuComponents = props => {
   return [
     {
       key: 'Tokens',
-      component: <Tokens key="Tokens" {...props} />,
-      menuItem: <Item icon="fas-coins" key="Tokens" text="Tokens" />
+      component: <Tokens key='Tokens' {...props} />,
+      menuItem: <Item icon='fas-coins' key='Tokens' text='Tokens' />
     },
     {
       key: 'Send/Receive BCH',
-      component: <SendReceive key="Send/Receive BCH" {...props} />,
+      component: <SendReceive key='Send/Receive BCH' {...props} />,
       menuItem: (
         <Item
-          icon="fa-exchange-alt"
-          key="Send/Receive BCH"
-          text="Send/Receive BCH"
+          icon='fa-exchange-alt'
+          key='Send/Receive BCH'
+          text='Send/Receive BCH'
         />
       )
     },
     {
       key: 'Wallet',
-      component: <Wallet key="Wallet" {...props} />,
-      menuItem: <Item icon="fa-wallet" key="Wallet" text="Wallet" />
+      component: <Wallet key='Wallet' {...props} />,
+      menuItem: <Item icon='fa-wallet' key='Wallet' text='Wallet' />
     },
     {
       key: 'Configure',
-      component: <Configure key="Configure" {...props} />,
-      menuItem: <Item icon="fas-cog" key="Configure" text="Configure" />
+      component: <Configure key='Configure' {...props} />,
+      menuItem: <Item icon='fas-cog' key='Configure' text='Configure' />
     },
     {
       key: 'TX History',
-      component: <TXHistory key="TX History" {...props} />,
+      component: <TXHistory key='TX History' {...props} />,
       menuItem: (
-        <Item icon="fas-cog" key="TX History" text="TX History" />
+        <Item icon='fas-cog' key='TX History' text='TX History' />
       )
     },
     {
       key: 'Demo Component',
-      component: <DemoComponent key="Demo Component" {...props} />,
+      component: <DemoComponent key='Demo Component' {...props} />,
       menuItem: (
-        <Item icon="fas-cog" key="Demo Component" text="Demo Component" />
+        <Item icon='fas-cog' key='Demo Component' text='Demo Component' />
       )
     }
   ]
